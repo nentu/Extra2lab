f4070f1438e8db0f2ea9663b92f7a0b7d97ed2dd
nentu <61655092+nentu@users.noreply.github.com>
2022-12-09 21:18:50 +0600

    Add files via upload

---
+++ src/main/java/org/itmo/calculator/SpecialCalculator.java
@@ -0,0 +1,126 @@
+package org.itmo.calculator;
+
+import org.itmo.calculator.operations.*;
+
+public final class SpecialCalculator {
+    private String[] memory;
+    private String lastOperation = "0";
+    private final MathOperation[] OperationsList = new MathOperation[9];
+
+
+    public SpecialCalculator(int memoryCellsSize) {
+        this.memory = new String[ (memoryCellsSize < 0 || memoryCellsSize > 100) ? 0: memoryCellsSize ];
+        for (int i = 0; i < this.memory.length; i++)
+            this.memory[i] = "0";
+
+        OperationsList[0] = new Addition();
+        OperationsList[1] = new Division();
+        OperationsList[2] = new DivisionRemainder();
+        OperationsList[3] = new DoubleAddition();
+        OperationsList[4] = new DoubleSubtraction();
+        OperationsList[5] = new Exponentiation();
+        OperationsList[6] = new Multiplication();
+        OperationsList[7] = new MultiplicationDivision();
+        OperationsList[8] = new Subtraction();
+    }
+
+    public String calculate(String expression) {
+        expression = expression.replace("/", "\\") + " ";
+
+
+        while (expression.split(" ").length > 2){
+            String[] tmp = expression.split(" ", 4);
+            String first_expression = String.format("%s %s %s", tmp[0], tmp[1], tmp[2]);
+            String ans = calculateOneExpression(first_expression);
+            if (!ans.contains("ERROR")) {
+                expression = String.format("%s %s", ans, tmp[3]);
+
+            }
+            else {
+                expression = ans;
+                break;
+            }
+
+        }
+        expression = expression.strip();
+        this.lastOperation = expression;
+        return expression; // не забудьте заменить "" на результат операции
+    }
+
+    private String getFromMemory(String tIndex){
+        int index = Integer.parseInt(tIndex);
+        if (0 <= index && index < memory.length) {
+            return this.memory[index];
+        }
+        else{
+            return "ERROR_1";
+        }
+    }
+
+    private String calculateOneExpression(String expression){
+
+        String[] listWords = expression.split(" ", 3);
+        long num1;
+        long num2;
+
+        //get num1
+        if (!listWords[0].contains("M")) {
+            num1 = Long.parseLong(listWords[0]);
+        }
+        else{
+            String tmp = getFromMemory(listWords[0].replace("M", ""));
+            if (!tmp.equals("ERROR_1")){
+                num1 = Long.parseLong(tmp);
+            }
+            else return tmp;
+        }
+
+        //get Sign
+        String sign =  listWords[1];
+
+        //get num2
+        if (!listWords[2].contains("M")) {
+            num2 = Long.parseLong(listWords[2]);
+        }
+        else{
+            String tmp = getFromMemory(listWords[2].replace("M", ""));
+            if (!tmp.equals("ERROR_1")){
+                num2 = Long.parseLong(tmp);
+            }
+            else return tmp;
+        }
+
+        String ans = "0";
+
+        //Solve expression
+        for (int i =0; i < OperationsList.length; i++){
+            MathOperation operation = OperationsList[i];
+            if (sign.equals(operation.getSign())){
+                ans = operation.calculate(num1, num2);
+            }
+        }
+
+        return ans;
+    }
+
+    public String putLastResultInMemory(int memoryCellIndex) {
+        if (0 <= memoryCellIndex && memoryCellIndex < memory.length) {
+            this.memory[memoryCellIndex] = this.lastOperation;
+            return String.valueOf(lastOperation);
+        }
+        else{
+            return "ERROR_1";
+        }
+    }
+
+    public String resetMemory(int memoryCellIndex) {
+        // напишите здесь код
+        if (0 <= memoryCellIndex && memoryCellIndex < memory.length) {
+            this.memory[memoryCellIndex] = "0";
+            return "0";
+        }
+        else{
+            return "ERROR_1";
+        }
+    }
+}
---
+++ src/main/java/org/itmo/calculator/operations/Division.java
@@ -0,0 +1,13 @@
+package org.itmo.calculator.operations;
+import org.itmo.calculator.MathOperation;
+public class Division extends MathOperation {
+    private final String SIGN = "\\";
+
+    @Override
+    public String getSign(){return this.SIGN;}
+
+    @Override
+    public String calculate(long num1, long num2){
+        return (num2 != 0) ? String.valueOf(num1 / num2):"ERROR_0";
+    }
+}
